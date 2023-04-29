0d965468069e9641686fb3dd391432dcc8894f4e
DedLigma <dedligma@192.168.1.3>
2023-04-29 23:55:19 +0300

    http

--- main.cc
+++ main.cc
@@ -1,13 +1,35 @@
+#include <cstdlib>
 #include <drogon/drogon.h>
-#include <iostream>
+#include <fstream>
+
+typedef std::function<void(const drogon::HttpResponsePtr &)> Callback;
+
+void indexHandler(const drogon::HttpRequestPtr &request, Callback &&callback) {
+  Json::Value jsonBody;
+  jsonBody["message"] = std::time(nullptr);
+
+  // Формируем и отправляем ответ с JSON-объектом
+  auto response = drogon::HttpResponse::newHttpJsonResponse(jsonBody);
+  callback(response);
+}
+
+void changeHandler(const drogon::HttpRequestPtr &request, Callback &&callback) {
+  Json::Value jsonBody;
+
+  jsonBody["message"] =   " jigreiger";
+  auto response = drogon::HttpResponse::newHttpJsonResponse(jsonBody);
+  callback(response);
+}
+
 int main() {
 
-    std::cout << "server start12312312" << std::endl;
-    //Set HTTP listener address and port
-    drogon::app().addListener("127.0.0.14",3010);
-    //Load config file
-    //drogon::app().loadConfigFile("../config.json");
-    //Run HTTP framework,the method will block in the internal event loop
-    drogon::app().run();
-    return 0;
+  drogon::app()
+      .registerHandler("/state", &indexHandler, {drogon::Get})
+      .registerHandler("/change", &changeHandler, {drogon::Get})
+      .addListener("127.0.0.1", 3001)
+      .setThreadNum(8)
+      .enableServerHeader(false)
+      .run();
+
+  return EXIT_SUCCESS;
 }
